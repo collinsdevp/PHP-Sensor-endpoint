e73e7975f703bf86999b21400d6a4b0d7acf287e
colllz <ogbonna.collins@gmail.com>
2018-01-16 19:21:36 -0700

    change
    change

---
+++ creative_thinking.php
@@ -0,0 +1,95 @@
+<html>
+
+<style type="text/css">
+
+header{
+width:1340px;
+height:100px;
+background-color:black
+}
+
+
+#moto{
+width:1340px;
+height:40px;
+background-color:orange;
+display: block;
+float:left;
+clear:both;
+}
+
+nav{
+width:1340px;
+height:50px;
+background-color:white;
+display: block;
+}
+
+nav ul{
+list-style:none;
+}
+
+nav ul li a{
+float:left;
+text-decoration:none;
+color:black;
+padding:0px 40px;
+line-height:40px;
+}
+
+nav ul li a:hover{
+text-decoration:underline;
+}
+
+nav ul li a:active{
+text-decoration:underline;
+}
+
+#content{
+background-color:grey;
+float:left;
+width:1340px;
+height:100px;
+color:white;
+}
+
+footer{
+background-color:black;
+height:100px;
+font-family:calibri;
+width:1340px;
+}
+</style>
+<head></head>
+
+<body>
+<header><header>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<div id="moto"></div>
+<br>
+<nav>
+<ul>
+<li><a href="#">By Business</a></li>
+<li><a href="#">Categories</a></li>
+<li><a href="#">Brands</a></li>
+</ul>
+</nav>
+
+
+<div id=content>
+<br><br><br>
+ // CONTENT && PHP ANIMATION
+<br><br><br>
+</div>
+
+<br><br><br>
+
+
+<footer></footer>
+</body>
+
+</html>
